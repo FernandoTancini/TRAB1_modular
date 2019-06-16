70b591a963bd24498da68ac6d5361a84644c78a0
SuemyInagaki <39767675+SuemyInagaki@users.noreply.github.com>
2019-06-16 19:30:45 -0300

    Update PECASCAPTURADAS.c

--- INF1301-Trab03/TRAB3-2 (Peças Capturadas)/PECASCAPTURADAS.c
+++ INF1301-Trab03/TRAB3-2 (Peças Capturadas)/PECASCAPTURADAS.c
@@ -1,150 +1,192 @@
-/***************************************************************************
-*  $MCI Módulo de implementação: Módulo peças capturadas
+/*****************************************************************************************
+*  $MCI Módulo de implementação: Módulo Pecas Capturadas
+*  Arquivo gerado:            PecasCapturadas.c
+*  Letras identificadoras:       BAR
 *
-*  Arquivo gerado:              PECASCAPTURADAS.C
-*  Letras identificadoras:      BAR
+*  Projeto:               Disciplina INF 1301
 *
-*  Nome da base de software:    Exemplo de teste automatizado
+*  Autor:                 sipf - Suemy Inagaki Pinheiro Fagundes
+* 
+*  $HA Histórico de evolução:
+*  Versão  Autor    Data     Observações
+*  1       sipf   09/jun/2019 início desenvolvimento
 *
-*  Projeto: INF 1301
-*  Autor: jfr     Júlia Affonso Figueiredo Rocha
+*       $ED Descrição do módulo
+*       Define as Funções de PecasCapturadas.
+*       No início de cada partida não existe PecasCapturadas
 *
-*  $HA Historico de evolucao:
-*     Versao   Autor    Data       Observacoes
-*       1.00   jfr   07/06/2019 Inicio do desenvolvimento
-*
-*  $ED Descricao do modulo
-*     O módulo implementa as funcoes de existencia e manutencao da Barra de Pecas Capturadas.
-*
-***************************************************************************/
+*****************************************************************************************/
 
-#include   <stdlib.h>
+
+#include   <malloc.h>
 #include   <stdio.h>
 
-#define PECASCAPTURADAS_OWN
+#define PECASCapturadas_OWN
 #include "PECASCAPTURADAS.h"
-#undef PECASCAPTURADAS_OWN
-#include "PECA.h"
-#include "LISTA.H"
+#undef PECASCapturadas_OWN
 
 /***********************************************************************
 *
-*  $TC Tipo de dados: BAR Descritor da barra de peça capturadas
+*  $TC Tipo de dados: BAR Descritor da peça capturada
+*
 *
 ***********************************************************************/
 
 typedef struct BAR_tagPecasCapturadas {
 
-	LIS_tppLista listaPecas;
-	/* Lista de Peças Capturadas */
-    
-    int tamanho;
-    /* Número de elementos na Barra*/
+    LIS_tppLista listaPecasVermelhas;
+    /* Lista de peças Vermelhas Capturadas */
 
-} BAR_tpPecasCapturadas;
+    LIS_tppLista listaPecasPretas;
+    /* Lista de peças pretas Capturadas */
+
+};
+
+/*****  Dados encapsulados no módulo  *****/
+
+/***** Protótipos das funções encapsuladas no módulo *****/
+
+void LiberarPeca(void *pPeca);
 
 /*****  Código das funções exportadas pelo módulo  *****/
 
-
 /***************************************************************************
 *
-*  Função: BAR Criar lista de peças capturadas
-*
-*/
-BAR_tpCondRet BAR_CriarListaPecasCapturadas(BAR_tpPecasCapturadas **pPecasCapturadas){
+*  Função: BAR Criar lista de peças Capturadas
+* 
+***************************************************************************/
+BAR_tpCondRet BAR_CriarListaPecasCapturadas(BAR_tpPecasCapturadas *pPecasCapturadas)
+{
 
-	*pPecasCapturadas = (BAR_tpPecasCapturadas *) malloc(sizeof(BAR_tpPecasCapturadas));
-    
-	if(*pPecasCapturadas == NULL) {
-        
-		return BAR_CondRetFaltouMemoria ;
-        
-	} /* if */
-    
-	(*pPecasCapturadas)->listaPecas = LIS_CriarLista(PEC_DestruirPeca);
-    (*pPecasCapturadas)->tamanho = 0;
+    *pPecasCapturadas = (BAR_tpPecasCapturadas) malloc(sizeof(struct BAR_tagPecasCapturadas));
+    if(*pPecasCapturadas == NULL)
+        return BAR_SemMemoria ;
 
-	return BAR_CondRetOK ;
+    (*pPecasCapturadas)->listaPecasVermelhas = LIS_CriarLista(LiberarPeca);
+    (*pPecasCapturadas)->listaPecasPretas = LIS_CriarLista(LiberarPeca);
 
-}
+    return BAR_OK ;
+
+} /* Fim função: BAR Criar lista de peças Capturadas */
 
 /***************************************************************************
 *
 *  Função: BAR Inserir peça
-*
-*/
-BAR_tpCondRet BAR_InserirPeca(BAR_tpPecasCapturadas *pPecasCapturadas, tppPeca pPeca) {
-    
-    if(pPecasCapturadas->listaPecas == NULL) {
-        
-		return BAR_CondRetListaPecasCapturadasNaoExiste;
-        
-	} /* if */
+*  
+****************************************************************************/
+BAR_tpCondRet BAR_InserirPeca(BAR_tpPecasCapturadas pPecasCapturadas, tppPeca pPeca)
+{
+    PEC_CorDaPeca  CorPeca;
 
-    if (LIS_InserirElementoAntes(pPecasCapturadas->listaPecas, pPeca) != LIS_CondRetOK) {
-        
-        return BAR_CondRetErro;
-    }
+    if(pPecasCapturadas == NULL) 
+        return BAR_NaoExisteLista;
 
-    pPecasCapturadas->tamanho++;
+    PEC_ObterCor(pPeca, &CorPeca);  
     
-	return BAR_CondRetOK;
+    if(CorPeca == PEC_corVermelha)
+        LIS_InserirElementoAntes(pPecasCapturadas->listaPecasVermelhas, pPeca);
 
-}
+    else
+        LIS_InserirElementoAntes(pPecasCapturadas->listaPecasPretas, pPeca);
+    
+    return BAR_OK;
 
-/***************************************************************************
-*
-*  Função: BAR Remover peça
-*
-*/
-BAR_tpCondRet BAR_RemoverPeca(BAR_tpPecasCapturadas *pPecasCapturadas) {
-    
-    if (pPecasCapturadas->listaPecas == NULL) {
-        return BAR_CondRetVazia;
-    }
-    
-    if(LIS_ExcluirElemento(pPecasCapturadas->listaPecas) != LIS_CondRetOK) {
-        
-        return BAR_CondRetErro;
-    }
-    pPecasCapturadas->tamanho--;
-    
-    return BAR_CondRetOK;
-}
+} /* Fim função: BAR Inserir peça */
 
 /***************************************************************************
 *
 *  Função: BAR Conta peças
-*
-*/
-BAR_tpCondRet BAR_ContarPecas(BAR_tpPecasCapturadas *pPecasCapturadas, int *pContagem) {
+*  
+***************************************************************************/
 
+   BAR_tpCondRet BAR_ContarPecas(BAR_tpPecasCapturadas pPecasCapturadas, PEC_CorDaPeca  CorPeca, int *pContagem)
+{
+    LIS_tppLista ListaPecas;
 
-    if(pPecasCapturadas->listaPecas == NULL) {
-		return BAR_CondRetListaPecasCapturadasNaoExiste;
-	} /* if */
+    if(pPecasCapturadas == NULL) 
+        return BAR_NaoExisteLista;
 
-    *pContagem = pPecasCapturadas->tamanho;
+    *pContagem = 0;
 
-	return BAR_CondRetOK ;
+    if(CorPeca == PEC_corVermelha)
+        ListaPecas = pPecasCapturadas->listaPecasVermelhas;
+    else 
+        ListaPecas = pPecasCapturadas->listaPecasPretas;
 
-}
+    IrInicioLista(ListaPecas);
+
+    if(LIS_ObterValor(ListaPecas) != NULL)
+        do
+        {
+            (*pContagem)++;
+        } while(LIS_AvancarElementoCorrente(ListaPecas, 1) == LIS_CondRetOK);
+
+    return BAR_OK ;
+
+} /* Fim função: BAR Contar peças */
 
 /***************************************************************************
 *
-*  Função: BAR Destruir lista de peças capturadas
-*
-*/
-BAR_tpCondRet BAR_DestruirListaPecasCapturadas(BAR_tpPecasCapturadas *pPecasCapturadas) {
-    
-    LIS_DestruirLista(pPecasCapturadas->listaPecas);
-    
-	free(pPecasCapturadas);
-    
-	pPecasCapturadas = NULL;
+*  Função: BAR Destruir lista de peças Capturadas
+*  
+****************************************************************************/
+BAR_tpCondRet BAR_DestruirListaPecasCapturadas(BAR_tpPecasCapturadas *pPecasCapturadas)
+{
+    if(*pPecasCapturadas == NULL || ((*pPecasCapturadas)->listaPecasVermelhas == NULL && (*pPecasCapturadas)->listaPecasPretas == NULL)) 
+        return BAR_NaoExisteLista;
 
-	return BAR_CondRetOK ;
+    if((*pPecasCapturadas)->listaPecasVermelhas != NULL)
+        LIS_DestruirLista((*pPecasCapturadas)->listaPecasVermelhas);
 
+    if((*pPecasCapturadas)->listaPecasPretas != NULL)
+        LIS_DestruirLista((*pPecasCapturadas)->listaPecasPretas);
+
+    free(*pPecasCapturadas);
+    *pPecasCapturadas = NULL;
+
+    return BAR_OK ;
+
+} /* Fim função: BAR Destruir lista de peças Capturadas */
+
+
+BAR_tpCondRet BAR_RemoverPeca(BAR_tpPecasCapturadas pPecasCapturadas, PEC_CorDaPeca  CorPeca) {
+
+if (CorPeca == PEC_corPreta)
+{
+    if (pPecasCapturadas->listaPecasPretas == NULL) {
+        return BAR_CondRetVazia;
+    }
+    if(LIS_ExcluirElemento(pPecasCapturadas->listaPecasPretas) != LIS_CondRetOK) {
+    return BAR_SemMemoria;
+    }
+}
+else{
+
+    if (pPecasCapturadas->listaPecasVermelhas == NULL) {
+        return BAR_CondRetVazia;
+    }
+
+    if(LIS_ExcluirElemento(pPecasCapturadas->listaPecasPretas) != LIS_CondRetOK) {
+    
+        return BAR_SemMemoria;
+    }
+}
+return BAR_OK;
 }
 
-/********** Fim do módulo de implementação: Módulo Peças Capturadas **********/
+/*****  Código das funções encapsuladas no módulo  *****/
+
+/***********************************************************************
+*
+*  $FC Função: Libera peça
+*
+*  $ED Descrição da função
+*     Libera uma peça.
+*
+***********************************************************************/
+void LiberarPeca(tppPeca *pPeca)
+{
+    PEC_DestruirPeca(&(*pPeca));
+}
+
+/********** Fim do módulo de implementação: Módulo peças Capturadas **********/
