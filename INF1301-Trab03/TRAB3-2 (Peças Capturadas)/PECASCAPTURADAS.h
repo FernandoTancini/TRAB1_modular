cd38a25367d2ebc65008f0ca921de5f6123412ed
SuemyInagaki <39767675+SuemyInagaki@users.noreply.github.com>
2019-06-16 19:31:20 -0300

    Update PECASCAPTURADAS.h

--- INF1301-Trab03/TRAB3-2 (Peças Capturadas)/PECASCAPTURADAS.h
+++ INF1301-Trab03/TRAB3-2 (Peças Capturadas)/PECASCAPTURADAS.h
@@ -1,177 +1,147 @@
-#if ! defined(PECASCAPTURADAS_)
-#define PECASCAPTURADAS_
+#if ! defined(PECASCapturadas_)
+#define PECASCapturadas_
 
 #include "PECA.h"
-/***************************************************************************
- *
- *  $MCD Modulo de definicao: Módulo peças capturadas
- *
- *  Arquivo gerado:              PECASCAPTURADAS.H
- *  Letras identificadoras:      BAR
- *
- *  Nome da base de software:    Exemplo de teste automatizado
- *
- *  Projeto: INF 1301
- *  Autor: jfr     Júlia Affonso Figueiredo Rocha
- *
- *  $HA Historico de evolucao:
- *     Versao   Autor    Data       Observacoes
- *       1.00   jfr   07/06/2019 Inicio do desenvolvimento
- *
- *  $ED Descricao do modulo
- *     O módulo implementa as funcoes de existencia e manutencao da Barra de Pecas Capturadas.
- *
- ***************************************************************************/
 
-#if defined(PECASCAPTURADAS_OWN)
-    #define PECASCAPTURADAS_EXT
+#include "LISTA.h"
+
+/***************************************************************************
+*
+*  $MCD Módulo de definição: Módulo peças Capturadas
+*
+*  Arquivo gerado:              PecasCapturadas.h
+*  Letras identificadoras:      BAR
+*
+*  Projeto: Disciplina INF 1301
+*
+*  Autores: sipf - Suemy Inagaki Pinheiro Fagundes
+*
+*  $HA Histórico de evolução:
+*     Versão       Autor          Data          Observações
+*       1.00       sipf        09/jun/2019   Início do desenvolvimento
+*
+*  $ED Descrição do módulo
+*     Este módulo implementa funções do módulo de peças Capturadas
+*     Não existe peças Capturadas no inicio do programa
+*
+***************************************************************************/
+ 
+#if defined(PECASCapturadas_OWN)
+   #define PECASCapturadas_EXT
 #else
-    #define PECASCAPTURADAS_EXT extern
+   #define PECASCapturadas_EXT extern
 #endif
 
-/* Tipo referência para a lista das Peças Capturadas */
+/***** Declarações exportadas pelo módulo *****/
 
-typedef struct BAR_tagPecasCapturadas BAR_tpPecasCapturadas;
+/* Tipo referência para uma lista de peças Capturadas */
 
-/***********************************************************************
- *
- *  $TC Tipo de dados: BAR Condições de retorno
- *
- ***********************************************************************/
-
-typedef enum {
-    
-    BAR_CondRetOK = 0,
-    /* Executou correto */
-    
-    BAR_CondRetErro = 1,
-    /* Erro na execução */
-    
-    BAR_CondRetListaPecasCapturadasNaoExiste = 2,
-    /* Lista de peças capturadas não existe */
-    
-    BAR_CondRetVazia = 3,
-    /* Lista de peças capturadas não contém elementos */
-    
-    BAR_CondRetFaltouMemoria = 4
-    /* Faltou memória ao alocar dados */
-    
-} BAR_tpCondRet;
-
+typedef struct BAR_tagPecasCapturadas* BAR_tpPecasCapturadas;
 
 /***********************************************************************
- *
- *  $FC Função: BAR Criar lista de peças capturadas
- *
- *  $ED Descrição da função
- *     Cria uma nova lista de peças capturadas.
- *
- *  $EP Parâmetros
- *     $P pPecasCapturadas - ponteiro para a lista de peças capturadas a ser criada.
- *
- *  $FV Valor retornado
- *     BAR_CondRetOK
- *     BAR_CondRetFaltouMemoria
- *
- *  Assertivas de Entrada: Deve existir um ponteiro para a lista a ser criada
- *  Assertivas de Saída:   Uma lista criada deve ser retornada pelo ponteiro recebido
- *                         no parâmetro
- *
- ***********************************************************************/
+*
+*  $TC Tipo de dados: BAR Condicoes de retorno
+*
+*
+***********************************************************************/
 
-BAR_tpCondRet BAR_CriarListaPecasCapturadas(BAR_tpPecasCapturadas **pPecasCapturadas);
+   typedef enum {
 
-/***********************************************************************
- *
- *  $FC Função: BAR Inserir peça
- *
- *  $ED Descrição da função
- *     Insere uma peça na lista de peças capturadas (Barra).
- *
- *  $EP Parâmetros
- *     $P pPecasCapturadas - ponteiro para a lista de peças capturadas.
- *     $P pPeca - ponteiro para a peça a ser adicionada na Barra.
- *
- *  $FV Valor retornado
- *     BAR_CondRetOK
- *     BAR_CondRetListaPecasCapturadasNaoExiste
- *     BAR_CondRetErro
- *
- *  Assertivas de Entrada: Deve existir um ponteiro para a lista de Pecas Capturadas.
- *  Assertivas de Saída:   A barra contém uma nova peça.
- *
- ***********************************************************************/
+         BAR_OK = 0,
+               /* Executou correto */
 
-BAR_tpCondRet BAR_InserirPeca(BAR_tpPecasCapturadas *pPecasCapturadas, tppPeca pPeca);
+		   BAR_NaoExisteLista = 1,
+			   /* Lista de peças Capturadas não existe */
+
+         BAR_SemMemoria = 2,
+               /* Faltou memória ao alocar dados */
+         BAR_CondRetVazia = 3,
+         /*Não tem peças na lista*/
+
+   } BAR_tpCondRet;
+
 
 /***********************************************************************
- *
- *  $FC Função: BAR Remover peça
- *
- *  $ED Descrição da função
- *     Remove uma peça na lista da barra.
- *
- *  $EP Parâmetros
- *     $P pPecasCapturadas - ponteiro para a lista de peças capturadas a ser utilizada.
- *
- *  $FV Valor retornado
- *     BAR_CondRetOK
- *     BAR_CondRetListaPecasCapturadasNaoExiste
- *     BAR_CondRetFaltouMemoria
- *
- *  Assertivas de Entrada: Deve existir um ponteiro para a lista de Pecas Capturadas.
- *  Assertivas de Saída:   A barra tem uma peca a menos.
- *
- ***********************************************************************/
+*
+*  $FC Função: BAR Criar lista de peças Capturadas
+*
+*  $ED Descrição da função
+*     Cria uma nova lista de peças Capturadas.
+*
+*  $EP Parâmetros
+*     $P pPecasCapturadas - ponteiro para a lista de peças Capturadas a ser criada. Passado por referência.
+*
+*  $FV Valor retornado
+*     BAR_OK
+*     BAR_SemMemoria
+*
+***********************************************************************/
 
-BAR_tpCondRet BAR_RemoverPeca(BAR_tpPecasCapturadas *pPecasCapturadas);
+   BAR_tpCondRet BAR_CriarListaPecasCapturadas(BAR_tpPecasCapturadas *pPecasCapturadas);
 
 /***********************************************************************
- *
- *  $FC Função: BAR Contar peças
- *
- *  $ED Descrição da função
- *     Conta as peças na barra.
- *
- *  $EP Parâmetros
- *     $P pPecasCapturadas - ponteiro para a lista de peças capturadas a ser utilizada.
- *     $P pContagem - ponteiro para o inteiro onde será armazenado o número de peças.
- *
- *  $FV Valor retornado
- *     BAR_CondRetOK
- *     BAR_CondRetListaPecasCapturadasNaoExiste
- *
- *  Assertivas de Entrada: Deve existir um ponteiro para a lista de Pecas Capturadas e um ponteiro para um inteiro onde será armazenado o numero de peças na barra.
- *  Assertivas de Saída:   O ponteiro de inteiro contém o número de peças na Barra.
- *
- ***********************************************************************/
+*
+*  $FC Função: BAR Inserir peça
+*
+*  $ED Descrição da função
+*     Insere uma peça na lista de peças Capturadas.
+*
+*  $EP Parâmetros
+*     $P pPecasCapturadas - ponteiro para a lista de peças Capturadas a ser utilizada.
+*     $P pPeca - ponteiro para a peça a ser utilizada.
+*
+*  $FV Valor retornado
+*     BAR_OK
+*     BAR_NaoExisteLista
+*	  BAR_SemMemoria
+*
+***********************************************************************/
 
-BAR_tpCondRet BAR_ContarPecas(BAR_tpPecasCapturadas *pPecasCapturadas, int *pContagem);
+   BAR_tpCondRet BAR_InserirPeca(BAR_tpPecasCapturadas pPecasCapturadas, tppPeca pPeca);
 
 /***********************************************************************
- *
- *  $FC Função: BAR Destruir lista de peças capturadas
- *
- *  $ED Descrição da função
- *     Destrói a lista de peças capturadas.
- *
- *  $EP Parâmetros
- *     $P pPecasCapturadas - ponteiro para a lista de peças capturadas a ser destruida.
- *
- *  $FV Valor retornado
- *     BAR_CondRetOK
- *     BAR_CondRetListaPecasCapturadasNaoExiste
- *
- *  Assertivas de Entrada: Deve existir um ponteiro para a lista de Pecas Capturadas.
- *  Assertivas de Saída:   A barra foi destruida.
- *
- ***********************************************************************/
+*
+*  $FC Função: BAR Contar peças
+*
+*  $ED Descrição da função
+*     Conta as peças de uma determinada cor na lista de peças Capturadas
+*
+*  $EP Parâmetros
+*     $P pPecasCapturadas - ponteiro para a lista de peças Capturadas a ser utilizada.
+*     $P CorPeca - cor das peças a serem contadas.
+*     $P pContagem - ponteiro para o inteiro onde será armazenado o número de peças.
+*
+*  $FV Valor retornado
+*     BAR_OK
+*	  BAR_NaoExisteLista
+*
+***********************************************************************/
 
-BAR_tpCondRet BAR_DestruirListaPecasCapturadas(BAR_tpPecasCapturadas *pPecasCapturadas);
+   BAR_tpCondRet BAR_ContarPecas(BAR_tpPecasCapturadas pPecasCapturadas, PEC_CorDaPeca  CorPeca, int *pContagem);
+
+/***********************************************************************
+*
+*  $FC Função: BAR Destruir lista de peças Capturadas
+*
+*  $ED Descrição da função
+*     Destrói a lista de peças Capturadas.
+*
+*  $EP Parâmetros
+*     $P pPecasCapturadas - ponteiro para a lista de peças Capturadas a ser utilizada. Passado por referência.
+*
+*  $FV Valor retornado
+*     BAR_OK
+*	  BAR_NaoExisteLista
+*
+***********************************************************************/
+
+   BAR_tpCondRet BAR_DestruirListaPecasCapturadas(BAR_tpPecasCapturadas *pPecasCapturadas);
+
+   BAR_tpCondRet BAR_RemoverPeca(BAR_tpPecasCapturadas pPecasCapturadas, PEC_CorDaPeca  CorPeca);
 
 #undef BAR_EXT
 
-/********** Fim do módulo de definição: Módulo peças capturadas **********/
+/********** Fim do módulo de definição: Módulo peças Capturadas **********/
 
 #else
 #endif
